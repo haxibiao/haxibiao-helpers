439ce5be3815c0c7ca5b90e08300871e4615f198
吴秀璋 <wuxiuzhang@haxibiao.com>
2020-06-03 22:31:09 +0800

    兼容答赚web

--- src/functions/admin.php
+++ src/functions/admin.php
@@ -66,3 +66,34 @@ function stick_video($data, $auto = false)
 {
     //以前置顶视频用
 }
+
+/*****************************
+ * *****答赚web网页兼容*********
+ * ***************************
+ */
+function get_seo_title()
+{
+    return \App\Seo::getValue('TKD', 'title');
+}
+
+function get_seo_keywords()
+{
+    return \App\Seo::getValue('TKD', 'keywords');
+}
+
+function get_seo_description()
+{
+    return \App\Seo::getValue('TKD', 'description');
+}
+
+function get_seo_meta()
+{
+    // $meta = '';
+    // if (Storage::exists("seo_config")) {
+    //     $json   = Storage::get('seo_config');
+    //     $config = json_decode($json);
+    //     $meta   = $config->seo_meta;
+    // }
+    // return $meta;
+    return \App\Seo::getValue('百度', 'meta');
+}

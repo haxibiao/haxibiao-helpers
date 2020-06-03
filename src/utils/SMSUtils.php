dcc6a89c94146e5499f32411f8bbde51239d4722
张志明 <zhangzhiming@haxifang.com>
2020-06-03 15:00:39 +0800

    up...

--- src/utils/SMSUtils.php
+++ src/utils/SMSUtils.php
@@ -54,7 +54,7 @@ class SMSUtils
             'channel' => $channel,
             'code'    => $code,
             'action'  => $action,
-            'actu'    => ""
+            'actu'    => "default"
         ]);
 
         //拼装数据

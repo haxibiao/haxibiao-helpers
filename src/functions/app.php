6e2bc356b103d0cbffe9700fe43b9552c1748f86
ivan <ivan@mac>
2021-01-09 21:20:31 +0800

    加入 copyStubs

--- src/functions/app.php
+++ src/functions/app.php
@@ -8,7 +8,32 @@ function getVodConfig(string $key)
     return config($name);
 }
 
-function stopfunction($name)
+/**
+ * 安装haxibiao下的packages到 app和nova下复制基础代码文件用
+ *
+ * @param string $pwd 当前代码目录
+ * @param boolean $force 是否强制
+ * @return void
+ */
+function copyStubs($pwd, $force = false)
 {
-    // \App\FunctionSwitch::close_function($name);
+    //复制所有app stubs
+    foreach (glob($pwd . '/stubs/*.stub') as $filepath) {
+        $filename = basename($filepath);
+        $dest     = app_path(str_replace(".stub", ".php", $filename));
+        if (!file_exists($dest) || $force) {
+            copy($filepath, $dest);
+        }
+    }
+    //复制所有nova stubs
+    if (!is_dir(app_path('Nova'))) {
+        mkdir(app_path('Nova'));
+    }
+    foreach (glob($pwd . '/stubs/Nova/*.stub') as $filepath) {
+        $filename = basename($filepath);
+        $dest     = app_path('Nova/' . str_replace(".stub", ".php", $filename));
+        if (!file_exists($dest) || $force) {
+            copy($filepath, $dest);
+        }
+    }
 }

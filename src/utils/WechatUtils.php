99fa11b9a2a000b65b8f606e899fb851e5dc0c20
ivan <ivan@haxibiao.com>
2021-01-19 16:53:54 +0800

    为Breeze重构基础模型

--- src/utils/WechatUtils.php
+++ src/utils/WechatUtils.php
@@ -3,12 +3,11 @@
 namespace Haxibiao\Helpers\utils;
 
 use App\Exceptions\UserException;
-use App\OAuth;
-use App\User;
-use App\Wallet;
 use GuzzleHttp\Client;
+use Haxibiao\Breeze\OAuth;
+use Haxibiao\Breeze\User;
 use haxibiao\helpers\utils\WechatMgUtils;
-
+use Haxibiao\Wallet\Wallet;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -210,10 +209,10 @@ class WechatUtils
                 'account'   => $user->account,
                 'unionid'   => $accessTokens['unionid'],
                 'openid'    => $accessTokens['openid'],
-            ]],200);
+            ]], 200);
         }
 
-        return successful_response($accessTokens,200);
+        return successful_response($accessTokens, 200);
     }
 
     /**

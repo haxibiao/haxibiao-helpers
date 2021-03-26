e191a835557ee7c039cb4f2f01ec05d2a148ad69
蔡志刚 <caizhigang@haxibiao.com>
2021-03-26 15:50:50 +0800

    add str_purify helper function

--- src/functions/str.php
+++ src/functions/str.php
@@ -10,3 +10,22 @@ function contains_chinese_text($str)
 {
     return preg_match("/[\x7f-\xff]/", $str);
 }
+
+/**
+ * 清理字符串中的HTML字符
+ */
+function str_purify($string) {
+
+	// ----- remove HTML TAGs -----
+	$string = preg_replace ('/<[^>]*>/', ' ', $string);
+
+	// ----- remove control characters -----
+	$string = str_replace("\r", '', $string);    // --- replace with empty space
+	$string = str_replace("\n", ' ', $string);   // --- replace with space
+	$string = str_replace("\t", ' ', $string);   // --- replace with space
+
+	// ----- remove multiple spaces -----
+	$string = trim(preg_replace('/ {2,}/', ' ', $string));
+
+	return $string;
+}
